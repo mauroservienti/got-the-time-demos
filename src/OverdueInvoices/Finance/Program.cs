179c88d66a54cd4521e26be254b9d4b26cf9c08d
Mauro Servienti <mauro.servienti@gmail.com>
2020-10-16 14:50:51 +0200

    Endpoint configuration

--- src/OverdueInvoices/Finance/Program.cs
+++ src/OverdueInvoices/Finance/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
 
 namespace Finance
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceName = typeof(Program).Namespace;
+            Console.Title = serviceName;
+
+            var config = new EndpointConfiguration(serviceName);
+            config.UseTransport<LearningTransport>();
+            config.UsePersistence<LearningPersistence>();
+            config.SendFailedMessagesTo("error");
+
+            var endpointInstance = await Endpoint.Start(config);
+
+            Console.WriteLine($"{serviceName} started. Press any key to stop.");
+            Console.ReadLine();
+
+            await endpointInstance.Stop();
         }
     }
 }
